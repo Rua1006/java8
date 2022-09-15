31660b399a1e18b2c23bcf9a82d5ea4f26a857ac
rua1006 <bskang0806@naver.com>
2022-09-15 17:25:58 +0900

    Ex

---
+++ ch04/src/sec4/Ex6.java
@@ -0,0 +1,27 @@
+package sec4;
+
+public class Ex6 {
+
+	public static void main(String[] args) {
+		for(int a=1;a<6;a++){
+			for(int b=0; b<a; b++){
+				System.out.print("*");
+							      }
+				System.out.println();
+							}
+	System.out.println("\n");
+		for(int a=0;a<5;a++){
+			for(int b=0;b<5-a;b++){
+			System.out.print("*");
+							      }
+			System.out.println();
+							}
+	System.out.println("\n");
+		for(int a=0;a<5;a++){
+			for(int b=5;b>a;b--){
+			System.out.print("*");
+								  }
+			System.out.println();
+							}
+											}
+				}
