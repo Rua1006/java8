6e49ce22aa262c10d18558f223b2203668ae9a52
rua1006 <bskang0806@naver.com>
2022-09-29 16:30:05 +0900

    java exception

---
+++ ch08/src/sec1/Clazz.java
@@ -0,0 +1,13 @@
+package sec1;
+//다중 상속
+public class Clazz implements Interface1, Interface2{
+
+	@Override
+	public void method() {				
+	}
+	@Override
+	public int method2(int pa1, String pa2) {
+		return 0;
+	}
+
+}
