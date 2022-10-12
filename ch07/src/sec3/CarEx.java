d88daa5b1c17322d8e044d26c65c732f4bc6801c
Rua1006 <bskang0806@naver.com>
2022-10-13 00:53:48 +0900

    pt

--- ch07/src/sec3/CarEx.java
+++ ch07/src/sec3/CarEx.java
@@ -1,33 +1,31 @@
 package sec3;
 
 public class CarEx {
-
 	public static void main(String[] args) {
 		Car car1 = new Car();
 		
-		for(int i=1; i<10; i++){
+		for(int i=1;i<=10;i++){
 			int errorLocation = car1.run();
 			
-			switch(errorLocation){
+			switch(errorLocation) {
 				case 1:
-					System.out.println("앞왼쪽 바퀴  KumhoTire로 교체");
-					car1.frontLeftTire = new KumhoTire(12,"앞왼쪽");
+					System.out.println("앞쪽 왼쪽 바퀴 Michelin Tire로 교체");
+					car1.frontLeftTire = new MichelinTire("앞왼쪽", 12);
 					break;
 				case 2:
-					System.out.println("앞오른쪽 바퀴  KumhoTire로 교체");
-					car1.frontRightTire = new KumhoTire(7,"앞오른쪽");
+					System.out.println("앞쪽 오른쪽 바퀴 Hankook Tire로 교체");
+					car1.frontRightTire = new HankookTire("앞오른쪽", 4);
 					break;
 				case 3:
-					System.out.println("뒤왼쪽 바퀴  KumhoTire로 교체");
-					car1.backLeftTire = new KumhoTire(4,"뒤왼쪽");
+					System.out.println("뒤쪽 왼쪽 바퀴 Kumho Tire로 교체");
+					car1.backLeftTire = new KumhoTire("뒤왼쪽", 5);
 					break;
 				case 4:
-					System.out.println("뒤오른쪽 바퀴  KumhoTire로 교체");
-					car1.backRightTire = new KumhoTire(8,"뒤오른쪽");
+					System.out.println("뒤쪽 오른쪽 바퀴 Hankook Tire로 교체");
+					car1.backRightTire = new HankookTire("뒤오른쪽", 6);
 					break;
-					
 			}
+			System.out.println("****************************************");
 		}
 	}
-
 }
--- ch07/src/sec3/Tire.java
+++ ch07/src/sec3/Tire.java
@@ -2,24 +2,24 @@ package sec3;
 
 public class Tire {
 	//멤버 필드
-	public int maxRotation; //최대회전수
-	public int accumulatedRotation; //회전수
-	public String location; //위치
+	public int maxRotation;	//최대 회전수(타이어 수명)
+	public int accumulatedRotation;	//누적 회전수
+	public String location;
 	//생성자
-	public Tire(int maxRotation, String location){
+	public Tire(int maxRotation, String location) {
 		this.maxRotation = maxRotation;
 		this.location = location;
-		
 	}
-	//맴버 메서드
+	//멤버 메소드
 	public boolean roll(){
 		++accumulatedRotation;
-		if(accumulatedRotation<maxRotation){
-			System.out.println(location+"Tire 수명:"+(maxRotation-accumulatedRotation));
+		if(accumulatedRotation<maxRotation) {
+			System.out.println(location + "Tire 수명 : "+(maxRotation-accumulatedRotation)+"회");
 			return true;
 		} else {
-			System.out.println("***"+location+"Tire 펑크****");
+			System.out.println("**** "+location+" Tire 펑크 ****");
 			return false;
-		} 
+		}
 	}
+	
 }
